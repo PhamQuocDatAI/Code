f7836f4241f1c66ca31a9cc372bc10ffe1c87604
unknown <edogawaconan0122@gmail.com>
2020-11-22 22:55:11 +0700

    message

--- Bai6.py
+++ Bai6.py
@@ -1,28 +1,31 @@
-# Tạo list gồm các số nguyên ngẫu nhiên trong đoạn cho trước
 import random
 n = int(input('Bạn muốn list có bao nhiêu phần tử: '))
 a = int(input('Nhập giá trị đầu trong đoạn: '))
 b = int(input('Nhập giá trị cuối trong đoạn: '))
-if a>b:
-    print('Bạn nhập giá trị đầu lớn hơn giá trị cuối:')
-    
-
+c = int(input('Step: '))
 list1 =[]
 list2 =[]
 if n<0:
     n==abs(n)
-
+if a==b:
+    print('Giá trị lớn nhất trong dãy là:',a)
 else:
-    while n>=0: 
-        d = random.randrange(a,b)
-# In ra các giá trị trong list được tạo ngẫu nhiên
-        list1.append(d)
-        list2.append(str(d))
-        n -=1
-print(','.join(list2))
-# Tìm giá trị lớn nhất trong list đã được tạo trên
-max = list1[0]
-for x in list1:
-    if max < x:
-        max = x
+    if a>b:
+        while n>=0:
+            d = random.randrange(b,a,c)
+            list1.append(d)
+            list2.append(str(d))
+            n -=1
+        print(','.join(list2))
+    else:
+        while n>=0:
+            d = random.randrange(a,b,c)
+            list1.append(d)
+            list2.append(str(d))
+            n -=1
+        print(','.join(list2))
+    max = list1[0]
+    for x in list1:
+        if max < x:
+            max = x
 print('Giá trị lớn nhất là:',max)
--- test.py
+++
@@ -1,8 +0,0 @@
-a = int(input('Nhập giá trị a: '))
-b = int(input('Nhập giá trị b: '))
-x=0
-if a > b:
-    x == a
-    a ==b
-    b==x
-print(a,b)
