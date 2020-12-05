1ac6c0fe3921ab1f30d3caa3dfc85033be99a4bc
unknown <edogawaconan0122@gmail.com>
2020-12-05 21:59:02 +0700

    Bai 13-2

--- Bài 13-2.py
+++ Bài 13-2.py
@@ -1,4 +1,8 @@
 import os
-file1 = open('file.txt', 'w+')
-file1.write('Python -- Python')
+vi_tri = input('Vị trí thư mục: ')
+ten_tap_tin = input('Nhập tên tập tin: ') 
+textfile = os.path.join(vi_tri, ten_tap_tin + '.txt')
+file1 = open(textfile, 'w+')
+file1.write(input('Viết gì đó không giấu nha ông nội ko là lỗi đó :D '))
 file1.close()
+
