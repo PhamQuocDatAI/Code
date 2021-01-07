f6c94c44a28fcdc72943eb4ddd4aacfd707de2e9
PhamQuocDatAI <edogawaconan0122@gmail.com>
2021-01-07 07:02:24 +0700

    QD

--- Phát triển 10-1.py
+++ Phát triển 10-1.py
@@ -1,2 +1,12 @@
 # Hãy tạo một ma trận số nguyên kích thước tùy ý và các giá trị là ngẫu nhiên.
+
+import numpy as np
+import random
+n = int(input("Ma trận cấp: "))
+A = np.random.randint(-10, 10, (n, n)) # Sinh số trọng phạm vi cho trước. => Không giởi hạn phạm vi -> Cho 2 số ngẫu nhiên 
+B = np.random.randint()
+print(A)
 # Hãy tạo một ma trận bất kì và thay thế những phần tử trong ma trận đó bằng các số ngẫu nhiên.
+
+
+
